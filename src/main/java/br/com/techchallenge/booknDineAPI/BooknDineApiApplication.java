fde9094c262d9e894c14601bc1624be8be22e8bd
Guilherme Ayusso <guilherme.ayusso@gmail.com>
2024-03-17 10:46:12 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/techchallenge/booknDineAPI/BooknDineApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.booknDineAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknDineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknDineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/techchallenge/booknDineAPI/BooknDineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.booknDineAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknDineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
